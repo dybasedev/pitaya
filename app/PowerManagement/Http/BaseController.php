ac981ba0b8dfb6b57e37597be4006cae6bb6e94e
chongyi <xpz3847878@163.com>
2016-07-03 01:32:13 +0800

    调整

---
+++ app/PowerManagement/Http/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * BaseController.php
+ *
+ * Creator:         chongyi
+ * Create Datetime: 2016/7/3 0:18
+ */
+
+namespace CloudGo\PowerManagement\Http;
+
+
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+
+}
