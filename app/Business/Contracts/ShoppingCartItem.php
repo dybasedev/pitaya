9b81b76a7a05c034107e1c9e15eee4a7838e641f
chongyi <xpz3847878@163.com>
2016-07-31 15:55:25 +0800

    接口定义

---
+++ app/Business/Contracts/ShoppingCartItem.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ShoppingCartItem.php
+ *
+ * Creator:         chongyi
+ * Create Datetime: 2016/7/31 14:13
+ */
+
+namespace ActLoudBur\Business\Contracts;
+
+/**
+ * Interface ShoppingCartItem
+ *
+ * 购物车项接口
+ *
+ * @package ActLoudBur\Business\Contracts
+ */
+interface ShoppingCartItem
+{
+
+}
