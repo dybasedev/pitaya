e43d6b397c1fbcd3d2be42411b79ff8b9a2b44cb
chongyi <xpz3847878@163.com>
2016-09-27 11:51:06 +0800

    ApiResponse

---
+++ app/Base/Http/ApiResponse.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * ApiResponse.php
+ *
+ * Creator:    chongyi
+ * Created at: 2016/09/27 11:06
+ */
+
+namespace PitayaApplication\Base\Http;
+
+use Illuminate\Http\JsonResponse;
+
+/**
+ * Class ApiResponse
+ *
+ * Api 响应格式化模块
+ *
+ * @package PitayaApplication\Base\Http
+ */
+class ApiResponse extends JsonResponse
+{
+
+}
